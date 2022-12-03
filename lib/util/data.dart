eb1b33bb834b1fc8ef7b9de5a7ad515930194d79
MuhamadHerdif <107844917+MuhamadHerdif@users.noreply.github.com>
2022-12-04 06:56:05 +0700

    first commit

---
+++ lib/util/data.dart
@@ -0,0 +1,14 @@
+List furnitures = [
+  {
+    "name": "Kursi Kayu",
+    "img": "assets/Kursi.jpeg",
+  },
+  {
+    "name": "Meja Kaca",
+    "img": "assets/Meja.jpeg",
+  },
+  {
+    "name": "Lampu Dinding",
+    "img": "assets/Lampu.jpeg",
+  },
+];
